fdc733bb10f529eaf8d498601c5f65907a14563c
Mohamed Tawfik <motawfik98@outlook.com>
2021-04-04 20:12:09 +0200

    Added onDelete setNull for categories

--- models/transactions/transactions.go
+++ models/transactions/transactions.go
@@ -11,7 +11,7 @@ type Transaction struct {
 	UserID      uint            `json:"userID" form:"userID"`
 	User        usersModel.User `json:"user" validate:"required,nostructlevel"`
 	CategoryID  *uint           `json:"categoryID" form:"categoryID"`
-	Category    Category        `json:"category" validate:"required,nostructlevel"`
+	Category    Category        `json:"category" validate:"required,nostructlevel" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Amount      int             `json:"amount" form:"amount" validate:"required,gt=0"`
 	Title       string          `json:"title" form:"transactionTitle" gorm:"size:255" validate:"required"`
 	Type        bool            `json:"type" form:"type"`
